6936e43326bbb21d1d7ed506bed2377113e30b2c
Barjuh <142073865+Barjuh@users.noreply.github.com>
2024-04-02 12:59:42 +0200

    C01 - 100%

---
+++ C01 - 100%/ex01/ft_ultimate_ft.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbourach <mbourach@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 14:22:34 by mbourach          #+#    #+#             */
+/*   Updated: 2024/03/18 18:55:10 by mbourach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <stdio.h>*/
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+int	main (void)
+{
+	int	*********p_nbr;
+	int	********p_nbr2;
+	int	*******p_nbr3;
+	int	******p_nbr4;
+	int	*****p_nbr5;
+	int	****p_nbr6;
+	int	***p_nbr7;
+	int	**p_nbr8;
+	int	*p_nbr9;
+	int	nbr;
+
+	nbr = 21;
+	p_nbr9 = &nbr;
+	p_nbr8 = &p_nbr9;
+	p_nbr7 = &p_nbr8;
+	p_nbr6 = &p_nbr7;
+	p_nbr5 = &p_nbr6;
+	p_nbr4 = &p_nbr5;
+	p_nbr3 = &p_nbr4;
+	p_nbr2 = &p_nbr3;
+	p_nbr = &p_nbr2;
+
+	printf("%d\n", nbr);
+	ft_ultimate_ft(p_nbr);
+	printf("%d\n", nbr);
+	return (0);
+}*/
