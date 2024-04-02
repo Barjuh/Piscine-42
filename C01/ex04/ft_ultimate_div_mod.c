7c59c85882c92cc8e3396dd65d17efe2f1e076bf
Barjuh <142073865+Barjuh@users.noreply.github.com>
2024-04-02 13:03:48 +0200

    C01 - 100%

---
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbourach <mbourach@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 15:42:29 by mbourach          #+#    #+#             */
+/*   Updated: 2024/03/18 18:32:45 by mbourach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <stdio.h>*/
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	div;
+	int	mod;
+
+	if (b != 0)
+	{
+		div = *a / *b;
+		mod = *a % *b;
+		*a = div;
+		*b = mod;
+	}
+}
+/*
+int	main(void)
+{
+	int	i = 39;
+	int	j = 17;
+	int	*a = &i;
+	int	*b = &j;
+
+	printf("%d /",i);
+	printf(" %d\n",j);
+
+	ft_ultimate_div_mod(a, b);
+	
+	printf("div (*a) = %d\n",*a);
+	printf("mod (*b) = %d",*b);
+
+	return (0);
+}*/
