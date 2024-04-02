74af163605e578152f394dfa316c956943e87ab7
Barjuh <142073865+Barjuh@users.noreply.github.com>
2024-04-02 13:03:12 +0200

    C00 - 60%

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbourach <mbourach@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/13 20:28:57 by mbourach          #+#    #+#             */
+/*   Updated: 2024/03/15 09:47:26 by mbourach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_3num(char a, char b, char c)
+{
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, &c, 1);
+	if (a != '7' || b != '8' || c != '9')
+	{
+		write(1, ", ", 2);
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char	d;
+	char	e;
+	char	f;
+
+	d = '0';
+	while (d <= '7')
+	{
+		e = d + 1;
+		while (e <= '8')
+		{
+			f = e + 1;
+			while (f <= '9')
+			{
+				ft_3num(d, e, f);
+				f++;
+			}
+			e++;
+		}
+		d++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}*/
